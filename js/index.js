b8a966d9a2167825af8ccd378ce17f70b5514a21
HanSte <stecherthananja@outlook.de>
2023-06-13 20:44:14 +0200

    new

--- js/index.js
+++ js/index.js
@@ -20,19 +20,54 @@ span.onclick = function() {
 }
 
 
-$(document).ready(function(){
-  $('#hs-search').click(function(){
-      // toggleClass is used just for testing
-      $('.hs-menu-item').addClass('hide-item') 
-      $('.hs-search-form').addClass('active')
-      $('.hs-close').addClass('active')
-      $('#hs-search').hide()
-  })
-  $('.hs-close').click(function(){
-      // toggleClass is used just for testing
-      $('.hs-menu-item').removeClass('hide-item') 
-      $('.hs-search-form').removeClass('active')
-      $('.hs-close').removeClass('active')
-      $('#hs-search').show()
-  })
+const searchButton = document.querySelector("nav .desktop-nav .link-search");
+const closeButton = document.querySelector(".search-container .link-close");
+const desktopNav = document.querySelector(".desktop-nav");
+const searchContainer = document.querySelector(".search-container");
+const overlay = document.querySelector(".overlay");
+
+searchButton.addEventListener("click", () => {
+    desktopNav.classList.add("hide");
+    searchContainer.classList.remove("hide");
+    overlay.classList.add("show");
+})
+
+closeButton.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+overlay.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+
+// Mobile Version
+
+const menuIconContainer = document.querySelector("nav .menu-icon-container");
+const navContainer = document.querySelector(".nav-container");
+
+menuIconContainer.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+})
+
+
+const searchBar = document.querySelector(".mobile-search-container .search-bar");
+const nav = document.querySelector(".nav-container nav");
+const searchInput = document.querySelector(".mobile-search-container input");
+const cancelBtn = document.querySelector(".mobile-search-container .cancel-btn");
+
+searchInput.addEventListener("click", () => {
+    searchBar.classList.add("active");
+    nav.classList.add("move-up");
+    desktopNav.classList.add("move-down");
+})
+
+cancelBtn.addEventListener("click", () => {
+    searchBar.classList.remove("active");
+    nav.classList.remove("move-up");
+    desktopNav.classList.remove("move-down");
 })
