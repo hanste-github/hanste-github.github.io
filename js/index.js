3d2629238913581af51c1d63181c045c9765b186
HanSte <stecherthananja@outlook.de>
2023-07-06 18:56:54 +0200

    new

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const hsclosebutton = document.getElementById('hsclosebutton');
+const hsmodal = document.getElementById('modal');
+
+hsclosebutton.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
