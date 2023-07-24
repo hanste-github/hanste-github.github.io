d3f50204bb4ca83074a30eb67789d672a83b8eeb
HanSte <stecherthananja@outlook.de>
2023-07-24 16:19:13 +0200

    new

--- js/index.js
+++ js/index.js
@@ -17,3 +17,22 @@ function myFunction() {
     }
   }
 }
+
+//Tabs
+
+function openTab(evt, tabName) {
+  var i, tabContent, tabButtons;
+
+  tabContent = document.getElementsByClassName("tab");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  tabButtons = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
