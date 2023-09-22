a481bece915af8d7ed96bcc061ad259e419b20e5
HanSte <stecherthananja@outlook.de>
2023-09-22 21:40:45 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -16,16 +16,14 @@ function myFunction() {
   }
 }
 
-window.addEventListener("scroll", (event) => {
-  var hsdoc = document.documentElement;
-  var hstop = (window.pageYOffset || hsdoc.scrollTop) - (hsdoc.clientTop || 0);
-  var hsheader = document.getElementsByClassName("hs-navbar")[0];
-  if (hstop > "10px") {
+window.onscroll = function() {hsscrollFunction()};
+function hsscrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     hsheader.style.borderRadius = "0 0 0.375rem 0.375rem";
   } else {
     hsheader.style.borderRadius = "0.375rem";
   }
-});
+};
 
 // CookieModal
 var hsmodal = document.getElementById("hsModal");
