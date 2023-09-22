a36f7dfdeff7d38d91c45932f317a0cbd6e9226c
HanSte <stecherthananja@outlook.de>
2023-09-22 21:59:55 +0200

    new

--- js/index.js
+++ js/index.js
@@ -14,13 +14,12 @@ function myFunction() {
   }
 }
 
-var hsnavbar = document.getElementsByClassName("hs-navbar");
 window.onscroll = function() {hsscrollFunction()};
 function hsscrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    hsnavbar.style.borderRadius = "0 0 0.375rem 0.375rem";
+    document.getElementById('hs-navbar').style.borderRadius = "0 0 0.375rem 0.375rem";
   } else {
-    hsnavbar.style.borderRadius = "0.375rem";
+    document.getElementById('hs-navbar').style.borderRadius = "0.375rem";
   }
 };
 
