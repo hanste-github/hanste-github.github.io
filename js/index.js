ce93527f551d05bab9916eba8702ede89800df86
HanSte <stecherthananja@outlook.de>
2023-09-25 22:29:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,70 @@
-function myFunction() {
-  var input = document.getElementById('myInput');
-  var filter = input.value.toUpperCase();
-  var ul = document.getElementById("myUL");
-  var li = ul.getElementsByTagName('li');
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
+// CookieModal
+var hsmodal = document.getElementById("hsModal");
+var hscloseBtn = document.getElementsByClassName("hs-close")[0];
+var hsacceptBtn = document.getElementById("hsacceptBtn");
+
+hscloseBtn.onclick = function() {
+  hsmodal.style.display = "none";
+};
+
+window.onclick = function(event) {
+	if (event.target == hsmodal) {
+    hsmodal.style.display = "none";
+	}
+};
+
+window.onload = function() {
+	hsmodal.style.display = "block";
+  if (localStorage.getItem("akzeptiert") === "true") {
+    hsmodal.style.display = "none";
   }
+};
+
+if (localStorage.getItem("akzeptiert") === "true") {
+  hsactivateScript();
+  hsmodal.style.display = "none";
 }
+
+hsacceptBtn.onclick = function() {
+  localStorage.setItem("akzeptiert", "true");
+  hsactivateScript();
+  hsmodal.style.display = "none";
+}
+
+function hsactivateScript() {
+
+  // Hintergrundbild
+  document.addEventListener('DOMContentLoaded', function() {
+    var savedImageUrl = localStorage.getItem('backgroundImage');
+    if (savedImageUrl) {
+        document.body.style.backgroundImage = "url(" + savedImageUrl + ")";
+    }
+  });
+
+  document.getElementById('uploadForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var fileInput = document.getElementById('imageUpload');
+    var file = fileInput.files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function() {
+        document.body.style.backgroundImage = "url(" + reader.result + ")";
+        localStorage.setItem('backgroundImage', reader.result);
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+  });
+
+  document.getElementById('urlForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var imageUrl = document.getElementById('imageUrl').value;
+    document.body.style.backgroundImage = "url(" + imageUrl + ")";
+    localStorage.setItem('backgroundImage', imageUrl);
+
+  });
+};
 
 // Navbar
 window.onscroll = function() {hsscrollFunction()};
@@ -135,72 +187,4 @@ function hssearchclose2() {
   document.getElementById('hs-search-content').style.display = "none";
   var hssearchhide = document.getElementsByClassName("hs-search-hide");
   hssearchhide.style.display = "";
-};
-
-// CookieModal
-var hsmodal = document.getElementById("hsModal");
-var hscloseBtn = document.getElementsByClassName("hs-close")[0];
-var hsacceptBtn = document.getElementById("hsacceptBtn");
-
-hscloseBtn.onclick = function() {
-  hsmodal.style.display = "none";
-};
-
-window.onclick = function(event) {
-	if (event.target == hsmodal) {
-    hsmodal.style.display = "none";
-	}
-};
-
-window.onload = function() {
-	hsmodal.style.display = "block";
-  if (localStorage.getItem("akzeptiert") === "true") {
-    hsmodal.style.display = "none";
-  }
-};
-
-if (localStorage.getItem("akzeptiert") === "true") {
-  hsactivateScript();
-  hsmodal.style.display = "none";
-}
-
-hsacceptBtn.onclick = function() {
-  localStorage.setItem("akzeptiert", "true");
-  hsactivateScript();
-  hsmodal.style.display = "none";
-}
-
-function hsactivateScript() {
-
-  // Hintergrundbild
-  document.addEventListener('DOMContentLoaded', function() {
-    var savedImageUrl = localStorage.getItem('backgroundImage');
-    if (savedImageUrl) {
-        document.body.style.backgroundImage = "url(" + savedImageUrl + ")";
-    }
-  });
-
-  document.getElementById('uploadForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var fileInput = document.getElementById('imageUpload');
-    var file = fileInput.files[0];
-    var reader = new FileReader();
-
-    reader.onloadend = function() {
-        document.body.style.backgroundImage = "url(" + reader.result + ")";
-        localStorage.setItem('backgroundImage', reader.result);
-    }
-
-    if (file) {
-        reader.readAsDataURL(file);
-    }
-  });
-
-  document.getElementById('urlForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var imageUrl = document.getElementById('imageUrl').value;
-    document.body.style.backgroundImage = "url(" + imageUrl + ")";
-    localStorage.setItem('backgroundImage', imageUrl);
-
-  });
 };
