ca6068280693e64717921f09efe177ce7401ed49
HanSte <stecherthananja@outlook.de>
2023-10-05 12:45:45 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -69,7 +69,7 @@ function hsactivateScript() {
 // Navbar
 window.onscroll = function() {hsscrollFunction()};
 function hsscrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 10) {
     document.getElementById('hs-navbar').style.borderRadius = "0 0 0.375rem 0.375rem";
     document.getElementById('hs-navbar').style.padding = "5px";
     document.getElementById('hs-navbar').style.height = "45px";
